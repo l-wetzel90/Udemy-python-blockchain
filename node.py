0588ef4889607c58e318328460813c333032fe25
Loren Wetzel <l.wetzel900@gmail.com>
2022-02-17 17:58:41 -0600

    different structures

--- node.py
+++ node.py
@@ -53,14 +53,13 @@ class Node:
                     print('Added transaction')
                 else:
                     print('Transaction Failed')
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == '2':
                 self.blockchain.mine_block()
             elif user_choice == '3':
                 self.print_blockchain_elements()
             elif user_choice == '4':
-                verifier = Verification()
-                if verifier.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print('\nAll transactions are valid')
                 else:
                     print('\nThere are invalid transactions')
@@ -69,8 +68,7 @@ class Node:
                 waiting_for_input = False
             else:
                 print('Input was invalid, please pick a value from the list!')
-            verifier = Verification()
-            if not verifier.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.chain):
                 self.print_blockchain_elements()
                 print('Invalid blockchain!!')
                 # Break out of the loop
