d302195992f99cf0d550b6f0550702556f780021
Loren Wetzel <l.wetzel900@gmail.com>
2022-03-02 09:45:09 -0600

    making pep8 formatting changes

--- node.py
+++ node.py
@@ -59,7 +59,7 @@ def load_keys():
 @app.route('/balance', methods=['GET'])
 def get_balance():
     balance = blockchain.get_balance()
-    if balance != None:
+    if balance is not None:
         response = {
             'message': 'Fetched balance successfully.',
             'funds': balance
@@ -68,7 +68,7 @@ def get_balance():
     else:
         response = {
             'message': 'Loading balance failed.',
-            'wallet_set_up': wallet.public_key != None
+            'wallet_set_up': wallet.public_key is not None
         }
         return jsonify(response), 500
 
@@ -88,7 +88,11 @@ def broadcast_transaction():
         }
         return jsonify(response), 400
     success = blockchain.add_transaction(
-        values['recipient'], values['sender'], values['signature'], values['amount'], is_receiving=True)
+        values['recipient'],
+        values['sender'],
+        values['signature'],
+        values['amount'],
+        is_receiving=True)
     if success:
         response = {
             'message': 'Successfully added transaction.',
@@ -141,7 +145,7 @@ def broadcast_block():
 
 @app.route('/transaction', methods=['POST'])
 def add_transaction():
-    if wallet.public_key == None:
+    if wallet.public_key is None:
         response = {
             'message': 'No wallet set up.'
         }
@@ -188,7 +192,7 @@ def mine():
         response = {'message': 'Resolve conflicts first, block not added!'}
         return jsonify(response), 409
     block = blockchain.mine_block()
-    if block != None:
+    if block is not None:
         dict_block = block.__dict__.copy()
         dict_block['transactions'] = [
             tx.__dict__ for tx in dict_block['transactions']]
@@ -201,7 +205,7 @@ def mine():
     else:
         response = {
             'message': 'Adding a block failed.',
-            'wallet_set_up': wallet.public_key != None
+            'wallet_set_up': wallet.public_key is not None
         }
         return jsonify(response), 500
 
@@ -257,7 +261,7 @@ def add_node():
 
 @app.route('/node/<node_url>', methods=['DELETE'])
 def remove_node(node_url):
-    if node_url == '' or node_url == None:
+    if node_url == '' or node_url is None:
         response = {
             'message': 'No node found.'
         }
